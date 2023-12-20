a6ab41666531cbd1275d9ce38a240f43f5976765
VinayR-GitHub <153238516+VinayR-GitHub@users.noreply.github.com>
2023-12-20 23:57:00 +1100

    Update contour_integration_test.m
    Added comments to aid developer experience.

--- contour_integration_test.m
+++ contour_integration_test.m
@@ -1,5 +1,9 @@
-%Declare complex function.
-func = @(z) eval(input('f(z) = '))
+%Take user input.
+func = @(z) eval(input('f(z) = '));
+
+%Define accessory functions.
 gunc = @(theta) cos(theta) + 1i*sin(theta);
 hunc = @(theta) -sin(theta) + 1i*cos(theta);
+
+%Evaluation of the integral.
 val = integral(@(t) func(gunc(t)).*hunc(t),0,2*pi)
