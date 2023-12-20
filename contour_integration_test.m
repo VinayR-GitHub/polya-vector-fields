1770e9134c8e579d95da34dddce3e2f6df450ad8
VinayR-GitHub <vinayraghavan7@gmail.com>
2023-12-20 14:05:03 +1100

    modified:   contour_integration_test.m

--- contour_integration_test.m
+++ contour_integration_test.m
@@ -1,2 +1,5 @@
 %Declare complex function.
-func = @z exp(z)./z
+func = @(z) eval(input('f(z) = '))
+gunc = @(theta) cos(theta) + 1i*sin(theta);
+hunc = @(theta) -sin(theta) + 1i*cos(theta);
+val = integral(@(t) func(gunc(t)).*hunc(t),0,2*pi)
